36fc8ef6c0178543afa10133ad367ac13c2339e1
Maicol Gomez <lociamjgm@gmail.com>
2023-12-14 11:36:06 -0500

    Recibir el json con los datos

--- df_query.py
+++ df_query.py
@@ -1,7 +1,10 @@
 from langchain.agents.agent_types import AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+#from langchain.agents import create
 import os
+import pandas as pd
+import pandas_gpt
 
 os.environ['OPENAI_API_KEY'] = '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'
 
@@ -9,18 +12,28 @@ os.environ['OPENAI_API_KEY'] = 'sk-NjCDVgHiElkJlFErUSoQT3BlbkFJIWUTPWjJq4kkHAOaE
 import pandas as pd
 from langchain.llms import OpenAI
 
-df = pd.read_csv("titanic.csv", sep=',')
 
 
-def query():
+
+def query_df(df, question):
     agent = create_pandas_dataframe_agent(
         ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
         df,
-        verbose=False,
+        verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
         return_intermediate_steps= False
     )
 
-    r = agent.run("how many rows are there?")
+    r = agent.run(question)
 
     return r
+
+def pandas_gpt_query():
+    df = pd.read_csv('/titanic.csv')
+
+'''
+if __name__ == "__main__":
+    df = pd.read_csv("titanic.csv", sep=',')
+    r = query_df(df, "how many rows are there?")
+    print(r)
+'''
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Depends
 from getpass import getpass
 import os
 from langchain.llms import OpenAI
-from df_query import query
-from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.middleware.cors import CORSMiddleware
+import json
+from typing import List, Dict, Any
+import pandas as pd
+from df_query import query_df
 
 os.environ['OPENAI_API_KEY'] = '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'
 app = FastAPI()
@@ -24,7 +27,8 @@ app.verson = "1.0"
 
 
 
-
+class DatosEntrada:
+    mi_array: List[str]
 
 llm_davinci = OpenAI(
     model_name="text-davinci-003",
@@ -40,6 +44,17 @@ def get_question(question: str):
 def get_question():
     return llm_davinci("what is iA?")
 
-@app.get('/query_df', tags=['question'])
-def get_question_df():
-    return query()
+
+@app.post('/query_df_body', tags=['question'])
+def get_question_df_body(data: Dict[str, Any], question:str):
+    #d = json.load(data)
+    columns = data["columns"]
+    rows = data["rows"]
+
+    # Crear un DataFrame con las filas y las columnas proporcionadas
+    df = pd.DataFrame(rows, columns=[col['displayName'] for col in columns])
+    #df = pd.DataFrame(rows)
+    r= query_df(df, question)
+    #df.to_csv('datos.csv', index=False)
+
+    return r
